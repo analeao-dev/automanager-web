4bfc1a68bd684c51de5141cf17d950558c0abea5
Ana Leao <anaqueiroz.dev@gmail.com>
2025-06-05 11:35:29 -0300

    chore: add and configure react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes";
+
 function App() {
 	return (
 		<>
+			<RouterProvider router={router} />
 			<h1 className='text-3xl font-bold underline'>AutoManager 🚗</h1>
 		</>
 	);
